abaa6e1c05b996428be2715bb2626abf7d3ec5db
joshred83 <joshuapfarina@gmail.com>
2023-06-27 19:26:12 -0400

    added comments to basic_logit_roc_plot

--- Code/basic_logit_with_roc_plot.R
+++ Code/basic_logit_with_roc_plot.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(ggfortify)
 library(tidyverse)
 
+# Read and wrangle
 df <- read.csv("Data/kaggle_aps/train.csv", header = T, 
                stringsAsFactors = F, 
                row.names = "X") %>% 
@@ -12,18 +13,33 @@ df <- read.csv("Data/kaggle_aps/train.csv", header = T,
                  satisfaction=ifelse(satisfaction=="satisfied", 1,0)) %>% 
           select(!id) 
     
-
+# Train Logit
 m <- df %>% glm(satisfaction ~ ., "binomial", .)
 
+# Summary of Logit
 m %>% summary(.)
 
+# add a column for probability predictions
 df$preds <- predict(m, df, "response")
+
+# Create a Predict/True data.frame out of complete cases.
+ 
 ROCR.df = k.aps.clean[complete.cases(k.aps.clean), c("preds", "satisfaction")]
+
+# Create a ROCR prediction object
 preds = prediction(ROCR.df$preds, ROCR.df$satisfaction)
+
+# Create a ROCR performance object for the ROC curve
 perf <- performance(preds,"tpr","fpr" )
 
+# Create a ROCR performance object for the AUC, extract the value & round it. 
 auc <- round(performance(preds, "auc")@y.values[[1]], 3)
+
+# use autplot from ggfortify to make a pretty version of the 
+# Roc curve. Adds an annotation with the AUC value. 
 autoplot(perf) +
     annotate(geom="label", x=.90, y=.10, label=paste("AUC:", round(auc,3)), fill="white")
+
+# Saves the most recent ggplot object as a png
 ggsave(filename="basic_logit_roc_plot.png", path="Visualizations/", 
        width =6, height=5, units="in")
