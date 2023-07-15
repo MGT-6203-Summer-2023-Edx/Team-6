1d7efe28a2f1972e87761022aabdadc7b7209ac2
joshred83 <joshuapfarina@gmail.com>
2023-07-15 10:48:45 -0400

    preprocessing

--- Code/preprocessor.R
+++ Code/preprocessor.R
@@ -1,30 +1,78 @@
-rm(list = ls())
+#rm(list = ls())
 library(tidyverse)
 
-getmode = function(values) {
+getmode <-  function(values) {
     uniquev = unique(values)
     uniquev[which.max(tabulate(match(values, uniquev)))]
 }
 
 
-data = read.csv("Data/kaggle_aps/train.csv", header = TRUE) %>%
 
-    #dropping id and arrival delay columns
-    select(-X,-id,-Arrival.Delay.in.Minutes) %>%
-
-    # Define numerics, transform lognormal variables.
-    mutate(
-        Age = as.numeric(Age),
-        Flight.Distance = as.numeric(log(Flight.Distance)),
-        Departure.Delay.in.Minutes = log(1 + as.numeric(Departure.Delay.in.Minutes))
+preprocess <- function() {
+    
+    train_validate <- read.csv("Data/kaggle_aps/train.csv", header = T, stringsAsFactors = T) %>%
+        
+        #dropping id and arrival delay columns
+        select(-X, -id, -Arrival.Delay.in.Minutes) %>%
+        # Define numerics, transform lognormal variables.
+        mutate(
+            Age = as.numeric(Age),
+            Flight.Distance = as.numeric(log(Flight.Distance)),
+            Departure.Delay.in.Minutes = log(1 + as.numeric(Departure.Delay.in.Minutes)),
+            across(7:20, as.factor),
+            fold = sample(
+                c("train", "validation"),
+                n(),
+                replace = T,
+                prob = c(.7, .3)
+            ),
+            satisfaction = ifelse(satisfaction == "satisfied", 1, 0)
+        )
+    
+    test <- read.csv("Data/kaggle_aps/test.csv", header = T, stringsAsFactors = T) %>%
+        
+        #dropping id and arrival delay columns
+        select(-X, -id, -Arrival.Delay.in.Minutes) %>%
+        
+        # Define numerics, transform lognormal variables.
+        mutate(
+            Age = as.numeric(Age),
+            Flight.Distance = as.numeric(log(Flight.Distance)),
+            Departure.Delay.in.Minutes = log(1 + as.numeric(Departure.Delay.in.Minutes)),
+            across(7:20, as.factor),
+            fold = "test",
+            satisfaction = ifelse(satisfaction == "satisfied", 1, 0)
+        )
+    
+    
+    likerts <- colnames(train_validate[, 7:20])
+    modes <- train_validate %>%
+        filter(fold == "train") %>%
+        summarise(across(all_of(likerts), getmode))
+    
+    
+    for (col.name in likerts) {
+        train_validate[which(train_validate[col.name] == 0), col.name] <-
+            modes[[1, col.name]]
+        test[which(test[col.name] == 0), col.name] <-
+            modes[[1, col.name]]
+        
+    }
+    preproc = list(
+        train = train_validate %>%
+            filter(fold == "train") %>%
+            mutate(across(7:20, droplevels)) %>% 
+            select(-fold),
+        validate = train_validate %>%
+            filter(fold == "validation") %>%
+            mutate(across(7:20, droplevels)) %>% 
+            select(-fold),
+        test = test %>%
+            filter(fold == "test") %>%
+            mutate(across(7:20, droplevels)) %>% 
+            select(-fold)
     )
-
-likerts <- colnames(data[,7:20])
-modes <- data %>%
-    summarise(across(all_of(likerts), getmode))
-
-
-for (col.name in likerts){
-    data[which(data[col.name]==0), col.name] <- modes[[1, col.name]]
+    return(preproc)
 }
-preproc = list(data=data, modes=modes)
+
+#splits <- preprocess()
