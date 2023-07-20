767a17fbbe6d3cab962b26a7cfdc7c7f26a9caf1
joshred83 <joshuapfarina@gmail.com>
2023-07-19 21:09:07 -0400

    ignore

--- Code/svm.R
+++ Code/svm.R
@@ -2,7 +2,6 @@ source("Code/preprocessor.R")
 source("Code/roc_curves.R")
 library(kernlab)
 library(tidyverse)
-
 preprocessed <- preprocess()
 train <- preprocessed$train
 validate <- preprocessed$validate
@@ -45,3 +44,15 @@ m <-
     )
 
 plot_roc_auc_curve(validate, m, title = "SVM with C=100")
+
+svm.coefs <- data.frame(colSums(m@xmatrix[[1]] * m@coef[[1]]))
+colnames(svm.coefs) <- "Coefficient"
+svm.coefs <- svm.coefs %>% dplyr::arrange(Coefficient)
+
+ggplot(svm.coefs, aes(x = Coefficient, y = reorder(rownames(svm.coefs), abs(Coefficient)))) +
+    geom_bar(stat = "identity") +
+    #coord_flip() +
+    theme_minimal() +
+    labs(title = "Feature Importance",
+         x = "Coefficient",
+         y = "Features")
